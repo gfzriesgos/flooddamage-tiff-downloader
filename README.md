2f07d18222268f7f319203fbf58337c4169509a4
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2019-07-31 08:35:48 +0200

    Script to download the data from geomer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # flooddamage-tiff-downloader
 Program for downloading the tiff files for the flooddamage process.
+
+For the usage of the program take a look at the test.sh file.
---
+++ download_tiff.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+'''
+This script downloads tiff files from
+a geoserver web coverage service.
+'''
+
+import argparse
+import shutil
+import requests
+
+def download_tiff_from_url(base_url, coverage_id, destination_file):
+    url = base_url + 'CoverageId={0}'.format(coverage_id)
+    res = requests.get(url, verify=False, stream=True)
+
+    if res.status_code == 200:
+        with open(destination_file, 'wb') as f:
+            res.raw.decode_content = True
+            shutil.copyfileobj(res.raw, f)
+    else:
+        raise Exception('Invalid status code', res.status_code)
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(description='Downloads a tiff from Web Coverage Service')
+    argparser.add_argument('base_url', help='The base url / server for downloading the file')
+    argparser.add_argument('coverage_id', help='Identifier for the file to download')
+    argparser.add_argument('--destination_file', help='Output file name', default='output.tiff')
+    args = argparser.parse_args()
+
+    base_url = args.base_url
+    coverage_id = args.coverage_id
+    destination_file = args.destination_file
+
+
+    download_tiff_from_url(base_url, coverage_id, destination_file)
